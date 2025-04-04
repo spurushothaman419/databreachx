0ff63af85955246449aa9e143e0acf3aff3c3a9b
spurushothaman419 <spurushothaman419@gmail.com>
2025-04-04 09:15:27 -0700

    Full working backend and frontend

---
+++ frontend/src/main.jsx
@@ -0,0 +1,9 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return <div className="p-6 text-xl font-bold">Welcome to DataBreachX</div>;
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
