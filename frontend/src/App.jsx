f1e71d635102a73a5fef48886ae4d0599e974c14
spurushothaman419 <spurushothaman419@gmail.com>
2025-04-04 17:30:52 -0700

    Updates App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,9 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [message, setMessage] = useState("Connecting to API...");
-  const apiUrl = import.meta.env.VITE_API_URL;
-
+  
   useEffect(() => {
-    console.log("✅ ENV VITE_API_URL:", apiUrl); // Debug log
-
+    console.log("✅ ENV VITE_API_URL:", import.meta.env.VITE_API_URL); // Debug log
     fetch(`${apiUrl}/`)
       .then((res) => res.json())
       .then((data) => setMessage(data.message))
