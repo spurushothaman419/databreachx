f990ce68947c022f3f262f345ecc7b17c2ebbdfc
spurushothaman419 <spurushothaman419@gmail.com>
2025-04-06 00:18:20 -0700

    Updated main.jsx file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
-const App = () => {
-  return <div className="p-6 text-xl font-bold">Welcome to DataBreachX</div>;
-};
-
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
